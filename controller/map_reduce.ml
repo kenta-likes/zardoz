ddaa9e95c4f0b58125cb540a1bf27b9777e62423
朱 千慧 <shuchie@ake-sensatoshi-no-MacBook-Pro.local>
2013-11-07 02:15:45 -0500

    Map_reduce.map updated. Still need to check what we should do if a worker is taking too long to complete a task

--- controller/map_reduce.ml
+++ controller/map_reduce.ml
@@ -3,10 +3,26 @@ open Worker_manager
 
 (* TODO implement these *)
 let map (kv_pairs: (string * string) list) (map_filename:string) : (string * string) list = 
-	(*ToDo
-	 * 1. Initialize a mapper worker by calling Worker_manager.initialize_mappers
-	 * 2. 
-	 * *)
+	(* ? Is the kv_pairs the complete list from the inputfile? or partial ?
+	 * 1. Initialize a mapper worker by calling Worker_manager.initialize_mappers *)
+    let wm = Worker_manager.initialize_mappers map_filename in
+	(* 2. Sends individual unmapped (id, body) pairs tp available mappers *)
+	let rec helper pairs acc = 
+        match pairs with
+        (* Base case. Return the result*)
+        | [] -> acc@l
+        (* more tasks to do *)
+        | (k, v)::tl -> 
+            let kevin = Worker_manager.pop_worker wm in (* Kevin the mapper! *)
+            begin
+            	match Worker_manager.map kevin k v with (* Kevin goes to work*)
+            	| Some l -> (* success. push Kevin and move onto next task *) 
+            	    Worker_manager.push_worker wm kevin ; helper tl (acc@l)
+            	| None   -> (* dead kevin?? *)   helper pairs acc
+            end
+        in
+        helper kv_pairs []
+	(* *)
   failwith "Go back whence you came! Trouble the soul of my Mother no more!"
 
 let combine (kv_pairs: (string * string) list) : (string * string list) list = 
