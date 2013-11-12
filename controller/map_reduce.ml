3c6d7ae0430d88b49360c60927afefa15d7590d9
朱 千慧 <shuchie@ake-sensatoshi-no-MacBook-Pro.local>
2013-11-12 02:14:08 -0500

    map and combine updated.

--- controller/map_reduce.ml
+++ controller/map_reduce.ml
@@ -2,45 +2,85 @@ open Util
 open Worker_manager
 
 
-type map_or_reduce = Map | Reduce
+module T = Thread_pool
+module H = Hashtbl
+module WM = Worker_manager 
+type worker_type = Map | Reduce 
+type worker   =  MapperKevin of WM.mapper | ReducerKevin of WM.reducer
 
-let mrhelper (kv_list : ('a * 'b) list) (mor :map_or_reduce)  = 
+
+(* because default sleep function won't let me sleep for less than a second *)
+let zzz (sec: float) = ignore (Unix.select [] [] [] sec)
+
+(* Helper function that adds everything in kvlst to a hashtable. *)
+let rec makehash (acc: ('a, 'b) H.t) kvlst =
+      match kvlst with
+      | (k,v)::t -> (H.add acc k v); makehash acc t 
+      | []   -> acc
+
+
+(* send kevin to work. Kevin can be mapper or reducer *)
+let sendtowork kevin k v : 'a list option =
+  match kevin with
+    | Map    ->  WM.map kevin k v  
+    | Reduce ->  WM.reduce kevin k v
+
+
+(* todo  : ('a, 'b) H.t = ('a, 'b) Hashtbl.t
+ * wtype : worker_type 
+ * wm    : 'a WM.worker_manager *)
+let rec mrhelper todo wtype wm = 
   (*1. create thread pool *)
-  let tpool = Thread_pool.create 20 in (* how many threads? *)
-  (*2. create a hashtbl that stores the result. Needs to be thread-safe *)
-  let resultsTable = Hashtbl.create (2 * (List.length kv_pairs)) in 
-  (*3. create a hashtbl that stores unfinished kv pairs. Needs to be thread-safe *)
-  let unfinishedList = ref [] in
-  (*4. Loop through the kv_pairs list *)
-  let assigntask =
-    let rec helper =
-
-    in
-    helper 
-  in
-  let definejob k v () : unit = 
+  let tpool = T.create 30 in
+  (*2. create a list that stores the result. access should be thread-safe *)
+  let results = ref [] in 
+  let definejob wm k v () : unit = 
     (* pop a worker *)
-    
+    let kevin  = WM.pop_worker wm in
     (* do work *)
+    let result :'a list option = 
+      sendtowork wtype kevin k v
+      in 
+    match result with
+      | Some res -> (* Success *)
+        (**** MUTEX LOCK ****)
+        if (H.mem todo k) then begin 
+          (* update todo and results *) 
+          (H.remove todo k); 
+          (results := ((k, res) :: !results));
+          (**** MUTEX UNLOCK ****)
+          (* push a worker*)
+          WM.push_worker wm kevin
+        end
+        else (* someone else has alreay done this job. *)
+        (**** MUTEX UNLOCK ****) ()
+      | None     -> (* Failure *) ()
+  in
+  T.add_work (definejob k v) tpool;
+  (* Sleep for 0.1 seconds *)
+  zzz 0.1;
+  (* Check for undone job *)
+  if (H.length todo) = 0 && (List.length !results) then !results
+  else (* Not everything is done *)
+    mrhelper todo mor wm
 
-    (* push a worker*)
 
-    (* update hashtbl *)
 
 
 
 (* TODO implement these *)
 let map (kv_pairs: (string * string) list) (map_filename:string) : (string * string) list = 
-  mrhelper Map  
+  (*Create a hashtbl that stores unfinished kv pairs. 
+    Access needs to be thread-safe *)
+  let unfinished = makehash (H.create (2 * (List.length kv_list))) kv_list in
+  mrhelper todo Map (WM.initialize_mappers map_filename)
 
 
 
 
 
 
-	(* ? Is the kv_pairs the complete list from the inputfile? or partial ?
-
-	 * 1. Initialize a mapper worker by calling Worker_manager.initialize_mappers *)
+	 (* 1. Initialize a mapper worker by calling Worker_manager.initialize_mappers *)
   (*  let wm = Worker_manager.initialize_mappers map_filename in
 	(* 2. Sends individual unmapped (id, body) pairs tp available mappers *)
 	let rec helper pairs acc = 
@@ -58,29 +98,33 @@ let map (kv_pairs: (string * string) list) (map_filename:string) : (string * str
             end
         in
         helper kv_pairs []*)
-	(* 
-  failwith "Go back whence you came! Trouble the soul of my Mother no more!"*)
 
+
+(* Combine values with the same keys. This function won't preserve order. *)
 let combine (kv_pairs: (string * string) list) : (string * string list) list = 
-    (*  *)
+  let rec helper1 acc1 lst1 =
+    let helper2 k ini_acc lst2 : string list * (string * string) list = 
+      let f (v_lst, carryover) (k1, v1): string list * (string * string) list= 
+        (* v_lst : string list , carryover : (string * string) list*)
+        if k = k1 then (v1::v_lst, carryover) 
+        else (v_lst, (k1, v1)::carryover) in
+      List.fold_left f (ini_acc, []) lst2 in
+    match lst1 with
+    | (k,v)::t -> 
+      let (values, carryover) = (helper2 k [v] t) in
+      helper1 ((k, values)::acc1) carryover
+    | [] -> acc1
+  in
+  helper1 [] kv_pairs
 
-  failwith "You have been doomed ever since you lost the ability to love."
 
 let reduce (kvs_pairs : (string * string list) list) (reduce_filename : string) : (string * string list) list =
-  mrhelper Reduce
+  (*  Create a hashtbl that stores unfinished kv pairs. 
+      Access needs to be thread-safe *)
+  let todo = makehash (H.create (2 * (List.length kv_list))) kv_list in
+  mrhelper todo Reduce (WM.initialize_reducers reduce_filename)
 
 
-
-(* TODO implement these *)
-let map kv_pairs map_filename : (string * string) list = 
-  failwith "Go back whence you came! Trouble the soul of my Mother no more!"
-
-let combine kv_pairs : (string * string list) list = 
-  failwith "You have been doomed ever since you lost the ability to love."
-
-let reduce kvs_pairs reduce_filename : (string * string list) list =
-  failwith "The only thing necessary for evil to triumph is for good men to do nothing"
->>>>>>> c13b72314b2a8646f81a76227162e257198b838e
 
 let map_reduce app_name mapper_name reducer_name kv_pairs =
   let map_filename    = Printf.sprintf "apps/%s/%s.ml" app_name mapper_name  in
