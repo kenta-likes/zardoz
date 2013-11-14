bcd6a54008a64d4b69a432a140a974f2dfa7951b
Kenta <kl459@cornell.edu>
2013-11-13 22:20:05 -0500

    Worker now implemented

--- worker_server/worker.ml
+++ worker_server/worker.ml
@@ -5,6 +5,7 @@ let reducerTable = Hashtbl.create 20
 let mapperMutex = Mutex.create ()
 let reducerMutex = Mutex.create ()
 type init_type = MapInit | RedInit
+type job_type = MapJob | RedJob
 
 let send_response client response =
   let success = Connection.output client response in
@@ -15,44 +16,64 @@ let send_response client response =
     success
 
 let rec handle_request (client : Connection.connection) : unit =
-    let safe_add id str table mutex = 
-        Mutex.lock mutex;
-        Hashtbl.add table id str;
-        Mutex.unlock mutex in
-    let safe_remove id table mutex =
-        Mutex.lock mutex;
-        Hashtbl.remove table id;
-        Mutex.unlock mutex in
     let send_helper (id_option,str) initType = match initType with
         |MapInit -> send_response client (Mapper (id_option, str))
         |RedInit -> send_response client (Reducer (id_option, str)) in
-    let initHelper source table mutex initType = 
+    let init_helper source table mutex initType = 
         match Program.build source with
         |(Some id, str) -> 
-                safe_add id str table mutex;
-                if send_helper (Some id, str) initType then
-                    handle_request client
-                else
-                    safe_remove id table mutex
+                Mutex.lock mutex;
+                Hashtbl.add table id str;
+                Mutex.unlock mutex;
+
+                if send_helper (Some id, str) initType then(
+                    handle_request client)
+                else ()
         |(None, err) -> if send_helper (None, err) initType then
                             handle_request client
                         else () in
-    let safeCheck table mutex id =
-        Mutex.lock mutex;
-        Hashtbl.mem id in
-    let workHelper i
+    let map_helper id input =
+        Mutex.lock mapperMutex;
+        if (Hashtbl.mem mapperTable id) then (
+            Mutex.unlock mapperMutex;
+            match Program.run id input with
+            |Some result -> if send_response client (MapResults (id, result)) then
+                                handle_request client
+                            else ()
+            |None -> if send_response client (RuntimeError (id, "Runtime error in program")) then
+                        handle_request client
+                    else ()
+        )
+        else Mutex.unlock mapperMutex;
+                if (send_response client (InvalidWorker id)) then
+                    handle_request client
+                else () in
+    let reduce_helper id input = 
+        Mutex.lock reducerMutex;
+        if (Hashtbl.mem reducerTable id) then (
+            Mutex.unlock reducerMutex;
+            match Program.run id input with
+            |Some result -> if send_response client (ReduceResults (id, result)) then
+                                handle_request client
+                            else ()
+            |None -> if send_response client (RuntimeError (id, "Runtime error in program")) then
+                        handle_request client
+                    else ()
+        )
+        else Mutex.unlock reducerMutex;
+                if (send_response client (InvalidWorker id)) then
+                    handle_request client
+                else () in
     match Connection.input client with
     |Some v ->
-    begin
-        match (v : worker_request) with
-        | InitMapper source -> initHelper source mapperTable mapperMutex MapInit
-        | InitReducer source -> initHelper source reducerTable reducerMutex RedInit
-        | MapRequest (id, k, v) -> 
-          failwith "You won't go unrewarded."
-        | ReduceRequest (id, k, v) -> 
-          failwith "Really? In that case, just tell me what you need."
-    end
-        | None ->
-          Connection.close client;
-          print_endline "Connection lost while waiting for request."
+        begin
+            match (v : worker_request) with
+            | InitMapper source -> init_helper source mapperTable mapperMutex MapInit
+            | InitReducer source -> init_helper source reducerTable reducerMutex RedInit
+            | MapRequest (id, k, v) -> map_helper id (k,v)
+            | ReduceRequest (id, k, v) -> reduce_helper id (k,v)
+        end
+    | None ->
+      Connection.close client;
+      print_endline "Connection lost while waiting for request."
 
