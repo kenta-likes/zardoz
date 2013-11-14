2fd197037786746371da8f72467a3b0e71ba77a9
朱 千慧 <shuchie@ake-sensatoshi-no-MacBook-Pro.local>
2013-11-13 20:39:43 -0500

    It compiles! (map_reduce.ml)

--- controller/map_reduce.ml
+++ controller/map_reduce.ml
@@ -6,8 +6,6 @@ module T = Thread_pool
 module H = Hashtbl
 module WM = Worker_manager 
 module M = Mutex
-type worker_type = Map | Reduce 
-type worker   =  MapperKevin of WM.mapper | ReducerKevin of WM.reducer
 let mutex_map = M.create () 
 let mutex_red = M.create ()
 
@@ -17,29 +15,22 @@ let zzz (sec: float) = ignore (Unix.select [] [] [] sec)
 
 (* Helper function that adds everything in kvlst to a hashtable. *)
 let rec makehash (acc: ('a, 'b) H.t) kvlst =
-      match kvlst with
-      | (k,v)::t -> (H.add acc k v); makehash acc t 
-      | []   -> acc
-
-
-(* send kevin to work. Kevin can be mapper or reducer *)
-let sendtowork kevin k v : 'a list option =
-  match kevin with
-    | MapperKevin  m ->  WM.map m k v  
-    | ReducerKevin r ->  WM.reduce r k v
+  match kvlst with
+  | (k,v)::t -> (H.add acc k v); makehash acc t 
+  | []   -> acc
 
 
 (* define job for mapper *)
-let mapperjob wm1 k v results () : unit =
+let mapperjob (wm1: (WM.mapper WM.worker_manager)) k v todo results () : unit =
   (* pop a worker *)
-  let kevin  = WM.pop_worker wm in
+  let kevin  = WM.pop_worker wm1 in
   (* do work *)
   let result :'a list option = WM.map kevin k v in 
   match result with
     | Some res -> (* Success *) M.lock mutex_map ;
       if (H.mem todo k) then begin 
         (* update todo and results and push a worker *) 
-        (H.remove todo k); (results := ((k, res) :: !results));
+        (H.remove todo k); (results := (res@ !results));
         M.unlock mutex_map; WM.push_worker wm1 kevin
       end
       else (* someone else has alreay done this job. *)
@@ -47,61 +38,40 @@ let mapperjob wm1 k v results () : unit =
     | None     -> (* Failure, accum. # failure *) ()
 
 
-
-let reducerjob wm2 k vlist results () : unit =
+let reducerjob (wm2 : WM.reducer WM.worker_manager) k vlist todo results () : unit =
   (* pop a worker *)
-  let kevin  = WM.pop_worker wm in
+  let kevin  = WM.pop_worker wm2 in
   let result :'a list option = WM.reduce kevin k vlist in
   match result  with
   | Some res -> (*sucess*) M.lock mutex_red;
     if (H.mem todo k) then begin 
         (* update todo and results, and push a worker *) 
-        (H.remove todo k); (results := ((k, res) :: !results)); 
+        (H.remove todo k); (results := ((k,res) :: !results)); 
         M.unlock mutex_red; WM.push_worker wm2 kevin
       end
     else M.unlock mutex_red 
   | None -> ()
-    
+  
 
-
-(* todo  : ('a, 'b) H.t = ('a, 'b) Hashtbl.t
- * wtype : worker_type 
- * wm    : 'a WM.worker_manager *)
-let rec mrhelper todo wtype wm = 
+let map (kv_pairs: (string * string) list) (map_filename:string) : (string * string) list = 
   (*1. create thread pool *)
   let tpool = T.create 30 in
-  (*2. create a list that stores the result. access should be thread-safe *)
+  (*2. create a list that stores the result. thread-safe *)
   let results = ref [] in 
-  (*3. Assign job *)
-  match wm with
-  | MapWM wm1 -> 
-    let f k v =  T.add_work (mapperjob wm1 k v) tpool in H.iter f todo 
-  | RedWM wm2 -> 
-    let f k v =  T.add_work (reducerjob wm2 k v) tpool in H.iter f todo    
-  (*4. Sleep for 0.1 seconds *) zzz 0.1;
-  (*5. Check for undone jobs *)
-  if (H.length todo) = 0 then begin 
-    (*cleanup workers, and then threadpool.destroy*) 
-    match wm with
-    | MapWM wm1 -> begin WM.clean_up_workers wm1; T.destroy tp; !results end
-    | RedWM wm2 -> begin WM.clean_up_workers wm2; T.destroy tp; !results end
-  end
-  else (* Not everything is done *)
-    mrhelper todo wtype wm
-
-
-
-
-
-(* TODO implement these *)
-let map (kv_pairs: (string * string) list) (map_filename:string) : (string * string) list = 
-  (*Create a hashtbl that stores unfinished kv pairs. 
-    Access needs to be thread-safe *)
-  let unfinished = makehash (H.create (2 * (List.length kv_list))) kv_list in
-  mrhelper todo Map (WM.initialize_mappers map_filename)
-
-
-
+  (*3. create a hashtbl that stores unfinished tasks. thread-safe *)
+  let todo = makehash (H.create (2 * (List.length kv_pairs))) kv_pairs in
+  (*4. initialize worker manager *)
+  let wm1 = WM.initialize_mappers map_filename in  
+  let rec helper () : (string * string) list = 
+    (*5. Assign jobs *)
+    let f k v = T.add_work (mapperjob wm1 k v todo results) tpool in 
+    begin H.iter f todo;
+    (*6. Sleep for 0.1 seconds *) zzz 0.1;
+    (*7. Check for undone jobs *)
+    if (H.length todo) = 0 then 
+      (WM.clean_up_workers wm1; T.destroy tpool; !results) else helper ()
+    end in
+  helper ()
 
 
 (* Combine values with the same keys. This function won't preserve order. *)
@@ -123,11 +93,25 @@ let combine (kv_pairs: (string * string) list) : (string * string list) list =
 
 
 let reduce (kvs_pairs : (string * string list) list) (reduce_filename : string) : (string * string list) list =
-  (*  Create a hashtbl that stores unfinished kv pairs. 
-      Access needs to be thread-safe *)
-  let todo = makehash (H.create (2 * (List.length kv_list))) kv_list in
-  mrhelper todo Reduce (WM.initialize_reducers reduce_filename)
-
+  (*1. create thread pool *)
+  let tpool = T.create 30 in
+  (*2. create a list that stores the result. thread-safe *)
+  let results : (string * string list) list ref = ref [] in 
+  (*3. create a hashtbl that stores unfinished tasks. thread-safe *)
+  let todo = makehash (H.create (2 * (List.length kvs_pairs))) kvs_pairs in
+  (*4. initialize worker manager *)
+  let wm2 = WM.initialize_reducers reduce_filename in 
+  let rec helper () : (string * string list) list = 
+    (*5. Assign jobs *)
+    let f k v = T.add_work (reducerjob wm2 k v todo results) tpool in 
+    begin H.iter f todo;
+    (*6. Sleep for 0.1 seconds *) zzz 0.1;
+    (*7. Check for undone jobs *)
+    if (H.length todo) = 0 
+    then (WM.clean_up_workers wm2; T.destroy tpool; !results) 
+    else helper ()
+    end in
+  helper ()
 
 
 let map_reduce app_name mapper_name reducer_name kv_pairs =
