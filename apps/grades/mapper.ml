72e682019f21fed99c84b6422d23615105572f78
朱 千慧 <shuchie@ake-sensatoshi-no-MacBook-Pro.local>
2013-11-13 23:30:16 -0500

    apps/grades/reducer.ml is updated.

--- apps/grades/mapper.ml
+++ apps/grades/mapper.ml
@@ -3,6 +3,6 @@ let (key, value) = Program.get_input() in
 let lst = (List.map split_spaces (split_to_class_lst value)) in 
 let extract acc elm = 
   match elm with
-  | k::score::[] -> (k, float_of_string score)::acc
+  | k::score::[] -> (k, score)::acc
   | _ -> failwith "Why" in
 Program.set_output (List.fold_left extract [] lst)
--- apps/grades/reducer.ml
+++ apps/grades/reducer.ml
@@ -1,1 +1,13 @@
 open Util;;
+let (key, values) = Program.get_input() in
+let bf v1 v2 :int = 
+  let f1 = float_of_string v1 in let f2 = float_of_string v2 in
+  if f1 > f2 then 1 else if f1 = f2 then 0 else -1 in
+let sorted = List.sort bf values in 
+let size   = List.length sorted  in
+  if size mod 2 = 1 then Program.set_output (List.nth (size/2))
+  else
+  let g1 = float_of_string (List.nth (size/2)) in 
+  let g2 = float_of_string (List.nth (size/2 + 1)) in 
+  Program.set_output [string_of_float ((g1 +. g2) /. 2.)]
+
