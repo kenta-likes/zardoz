4f0498293cd6e91cee68c97022afb05eb8f3a7b7
朱 千慧 <shuchie@rrdhcp-10-32-95-240.redrover.cornell.edu>
2013-11-13 22:07:35 -0500

    test

--- apps/grades/grades.ml
+++ apps/grades/grades.ml
@@ -4,9 +4,15 @@ let main (args : string array) : unit =
   if Array.length args < 3 then
     print_endline "Usage: grades <filename>"
   else
-   (*TODO: Write Me! *)
-   failwith "You're in a merry mood today, Mr. Todd."
- 
+    let filename = args.(2) in
+    let students : student list = load_grades filename in
+    let kv_pairs = 
+      let f d = 
+        string_of_int d.id_num, d.course_grades in
+      List.rev_map f students in
+    let reduced = 
+      Map_reduce.map_reduce "grades" "mapper" "reducer" kv_pairs in
+    print_reduced_courses reduced 
 
 in
 
--- apps/grades/mapper.ml
+++ apps/grades/mapper.ml
@@ -1,1 +1,7 @@
 open Util;;
+let (key, value) = Program.get_input() in
+let lst =   in 
+Program.set_output lst 
+
+
+(List.map  (split_to_class_lst value))
