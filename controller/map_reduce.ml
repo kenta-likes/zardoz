413f1ca2277a04e88183df0d4c70bb5d6ae6ba3c
kentalabur <kl459@cornell.edu>
2013-11-14 23:29:06 -0500

    Update map_reduce.ml

--- controller/map_reduce.ml
+++ controller/map_reduce.ml
@@ -22,8 +22,14 @@ let rec makehash (acc: ('a, 'b) H.t) kvlst =
 
 
 (* define job for mapper *)
+<<<<<<< HEAD
+let mapperjob (wm1: (WM.mapper WM.worker_manager)) k v todo results ()  =
+  (* pop a worker *)
+  try begin
+=======
 let mapperjob (wm1: (WM.mapper WM.worker_manager)) k v todo results () : unit =
   (* pop a worker *) try 
+>>>>>>> 096c0ee231e1adb6dde34dd9755e9075d411c22d
   let kevin  = WM.pop_worker wm1 in
   (* do work *)
   let result :'a list option = WM.map kevin k v in 
@@ -36,13 +42,21 @@ let mapperjob (wm1: (WM.mapper WM.worker_manager)) k v todo results () : unit =
       end
       else (* someone else has alreay done this job. *)
         (M.unlock mutex_map; WM.push_worker wm1 kevin)
-    | None     -> (* Failure, accum. # failure *) ()
+    | None  -> (* Failure *) ()
+  end
+  with _ -> ()
 
     with _ -> ()
 
 
+<<<<<<< HEAD
+let reducerjob (wm2 : WM.reducer WM.worker_manager) k vlist todo results ()  =
+  (* pop a worker *)
+  try begin
+=======
 let reducerjob (wm2 : WM.reducer WM.worker_manager) k vlist todo results () : unit =
   (* pop a worker *) try
+>>>>>>> 096c0ee231e1adb6dde34dd9755e9075d411c22d
   let kevin  = WM.pop_worker wm2 in
   let result :'a list option = WM.reduce kevin k vlist in
   match result  with
@@ -54,53 +68,53 @@ let reducerjob (wm2 : WM.reducer WM.worker_manager) k vlist todo results () : un
       end
     else (M.unlock mutex_red; WM.push_worker wm2 kevin)
   | None -> ()
+<<<<<<< HEAD
+  end
+=======
   
+>>>>>>> 096c0ee231e1adb6dde34dd9755e9075d411c22d
   with _ -> ()
 
 
 (**************************** Main Functions ****************************)
 
-let map (kv_pairs: (string * string) list) map_filename : (string * string) list = 
+let map kv_pairs map_filename : (string * string) list = 
   (*1. create thread pool *)
   let tpool = T.create 50 in
   (*2. create a list that stores the result. thread-safe *)
   let results = ref [] in 
   (*3. create a hashtbl that stores unfinished tasks. thread-safe *)
-  let todo = makehash (H.create (2 * (List.length kv_pairs))) kv_pairs in
+  let todo = makehash (H.create (2 * (List.length kv_pairs)+1)) kv_pairs in
   (*4. initialize worker manager *)
   let wm1 = WM.initialize_mappers map_filename in  
   let rec helper () : (string * string) list = 
     (*5. Assign jobs *)
-    let f k v = T.add_work (mapperjob wm1 k v todo results) tpool in 
-    begin H.iter f todo;
-    (*6. Sleep for 0.1 seconds *) zzz 0.1;
+    let f k v = ignore(T.add_work (mapperjob wm1 k v todo results) tpool) in 
+    begin ignore(H.iter f todo);
+    (*6. Sleep for 0.1 seconds *) zzz 1.;
     (*7. Check for undone jobs *)
-    Mutex.lock mutex_map;
+    Mutex.lock mutex_map; 
     if (H.length todo) = 0 then 
-      (WM.clean_up_workers wm1; T.destroy tpool; Mutex.unlock mutex_map; !results)
+      (WM.clean_up_workers wm1; T.destroy tpool; 
+      Mutex.unlock mutex_map; !results)
       else (Mutex.unlock mutex_map; helper ())
     end 
   in
   helper ()
 
 
-(* Combine values with the same keys. This function won't preserve order. *)
 
+(* Combine values with the same keys. This function won't preserve order. *)
 let combine (kv_pairs: (string * string) list) : (string * string list) list = 
-  let rec helper1 acc1 lst1 =
-    let helper2 k ini_acc lst2 : string list * (string * string) list = 
-      let f (v_lst, carryover) (k1, v1): string list * (string * string) list= 
-        (* v_lst : string list , carryover : (string * string) list*)
-        if k = k1 then (v1::v_lst, carryover) 
-        else (v_lst, (k1, v1)::carryover) in
-      List.fold_left f (ini_acc, []) lst2 in
-    match lst1 with
-    | (k,v)::t -> 
-      let (values, carryover) = (helper2 k [v] t) in
-      helper1 ((k, values)::acc1) carryover
-    | [] -> acc1
-  in
-  helper1 [] kv_pairs 
+  let hash_in = makehash (H.create 113) kv_pairs in 
+  let hash_out = H.create 113 in 
+  let helper k v =
+    if (H.mem hash_out k) then H.replace hash_out k (v::(H.find hash_out k))
+    else H.add hash_out k (v::[]) 
+  in H.iter helper hash_in; 
+  H.fold (fun k v acc -> (k,v)::acc) hash_out []
+  
+
 
 
 let reduce kvs_pairs reduce_filename : (string * string list) list =
@@ -109,18 +123,19 @@ let reduce kvs_pairs reduce_filename : (string * string list) list =
   (*2. create a list that stores the result. thread-safe *)
   let results : (string * string list) list ref = ref [] in 
   (*3. create a hashtbl that stores unfinished tasks. thread-safe *)
-  let todo = makehash (H.create (2 * (List.length kvs_pairs))) kvs_pairs in
+  let todo = makehash (H.create (2 * (List.length kvs_pairs)+1)) kvs_pairs in
   (*4. initialize worker manager *)
   let wm2 = WM.initialize_reducers reduce_filename in 
   let rec helper () : (string * string list) list = 
     (*5. Assign jobs *)
-    let f k v = T.add_work (reducerjob wm2 k v todo results) tpool in 
-    begin H.iter f todo;
-    (*6. Sleep for 0.1 seconds *) zzz 0.1;
+    let f k v = ignore(T.add_work (reducerjob wm2 k v todo results) tpool) in 
+    begin ignore(H.iter f todo);
+    (*6. Sleep for 0.1 seconds *) zzz 1.;
     (*7. Check for unfinished jobs *)
-    Mutex.lock mutex_red;
+    Mutex.lock mutex_red; 
     if (H.length todo) = 0 
-    then (WM.clean_up_workers wm2; T.destroy tpool; Mutex.unlock mutex_red; !results) 
+    then (WM.clean_up_workers wm2; T.destroy tpool; Mutex.unlock mutex_red; 
+      !results) 
     else (Mutex.unlock mutex_red; helper ())
     end in
   helper ()
