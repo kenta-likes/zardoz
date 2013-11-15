e0ced57ad00df43f98b5c030f53ffc6a251f6689
Kenta <kl459@cornell.edu>
2013-11-14 20:28:33 -0500

    Small performance improvement

--- apps/transaction_track/reducer.ml
+++ apps/transaction_track/reducer.ml
@@ -1,9 +1,9 @@
 let unpack : string -> int*int = Util.unmarshal in
 let (key, values) = Program.get_input() in
-let rawVals = List.map (fun elem -> unpack elem) values in
-let compare (bid1, _) (bid2,_) = if bid1 < bid2 then -1
-                else if bid1 > bid2 then 1 else 0 in
-let sortedVals = List.map (fun elem -> snd elem) (List.sort compare rawVals) in
+let rawVals = List.rev_map (fun elem -> unpack elem) values in
+let compare (bid1, _) (bid2,_) = if bid1 < bid2 then 1
+                else if bid1 > bid2 then -1 else 0 in
+let sortedVals = List.rev_map (fun elem -> snd elem) (List.sort compare rawVals) in
 let sum = List.fold_left (fun acc v ->
     if v = 0 then
         0
