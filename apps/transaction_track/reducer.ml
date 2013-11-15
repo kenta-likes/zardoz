ed251274b149e33875e9b4208c89b6ff11812877
Kenta <kl459@cornell.edu>
2013-11-14 20:22:15 -0500

    Small edit to transactions

--- apps/transaction_track/reducer.ml
+++ apps/transaction_track/reducer.ml
@@ -3,10 +3,11 @@ let (key, values) = Program.get_input() in
 let rawVals = List.map (fun elem -> unpack elem) values in
 let compare (bid1, _) (bid2,_) = if bid1 < bid2 then -1
                 else if bid1 > bid2 then 1 else 0 in
+let sortedVals = List.map (fun elem -> snd elem) (List.sort compare rawVals) in
 let sum = List.fold_left (fun acc v ->
-    if snd v = 0 then
+    if v = 0 then
         0
     else
-        acc + (snd v) )
-    0 (List.sort compare rawVals) in
+        acc + v )
+    0 sortedVals in
     Program.set_output [string_of_int sum]
